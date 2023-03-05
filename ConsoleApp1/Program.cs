9546e8aac9bb6f97a245faeb6f50eb5994f8b5ca
GBFerro <giovani.ferro@usp.br>
2023-03-05 18:38:50 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, totalPar, totalImpar;
+
+        totalPar = 0;
+        totalImpar = 0;
+        n = 0;
+        while (n < 1000)
+        {
+            if ((n > 1000)&&(n > 0))
+            {
+                Console.WriteLine("Digite um número: ");
+                n = int.Parse(Console.ReadLine());
+
+                if (n % 2 == 0 )
+                {
+                    totalPar += n;
+                }
+                else
+                {
+                    totalImpar += n;
+                }
+            }
+
+
+        }
+            Console.WriteLine("A soma dos números pares é: " + totalPar + " E dos impares é: " + totalImpar);
+    }
+}
